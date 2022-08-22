e378e5dc7a9a89336657a2024a8cf19eb90b76a9
Saveliy <bizon1997mail@mail.ru>
2022-08-22 19:26:29 +0300

    html about-page

---
+++ index.js
@@ -0,0 +1,14 @@
+/* 
+1. GitFlow. Часть 1
+https://lk.result.school/pl/teach/control/lesson/view?id=251712754
+
+gitflow
+1. Создать репозиторий на гите и клонировать его на компьютер
+2. Создать ветку разработки develop от главной ветки (main, master)
+
+2. GitFlow. Часть 2
+https://lk.result.school/pl/teach/control/lesson/view?id=251712755
+
+git checkout -b feature/about-page
+
+*/
